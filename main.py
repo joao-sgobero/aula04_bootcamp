5909b63fc4f218aa05108465f2e425db6ecf72c6
João Henrique Sgobero <joao.sgobero@gmail.com>
2026-06-28 16:33:25 -0400

    Enviando resolução do exercicio 03

--- main.py
+++ main.py
@@ -14,11 +14,20 @@
 
 #===============================================
 
-# Dados dois dicionários, fundi-los em um único dicionário.
+#2) Dados dois dicionários, fundi-los em um único dicionário.
 
-dicionario1 = {"a": 1, "b": 2}
-dicionario2 = {"c": 3, "d": 4}
+# dicionario1 = {"a": 1, "b": 2}
+# dicionario2 = {"c": 3, "d": 4}
 
-dicionario_fundido = {**dicionario1, **dicionario2}
+# dicionario_fundido = {**dicionario1, **dicionario2}
 
-print(dicionario_fundido)
+# print(dicionario_fundido)
+
+#===============================================
+
+# 3) Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0.
+estoque = {"Teclado": 10, "Mouse": 0, "Monitor": 3, "CPU": 0}
+
+estoque_positivo = {produto: quantidade for produto, quantidade in estoque.items() if quantidade > 0}
+
+print(estoque_positivo)
