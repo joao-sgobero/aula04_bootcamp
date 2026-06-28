4a7158740bb5c42207b5ae371dee448a4825aab5
João Henrique Sgobero <joao.sgobero@gmail.com>
2026-06-28 13:11:54 -0400

    Enviando resolução do exercicio 2

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 # 1)  Dada uma lista de dicionários representando produtos, atualizar o preço de um produto específico.
-produtos = [
-    {"id": 1, "nome": "Teclado", "preço": 100},
-    {"id": 2, "nome": "Mouse", "preço": 80},
-    {"id": 3, "nome": "Monitor", "preço": 300}
-]
+# produtos = [
+#     {"id": 1, "nome": "Teclado", "preço": 100},
+#     {"id": 2, "nome": "Mouse", "preço": 80},
+#     {"id": 3, "nome": "Monitor", "preço": 300}
+# ]
 
-# Atualizar o preço do produto com id 2 para 90
-for produto in produtos:
-    if produto["id"] == 2:
-        produto["preço"] = 90
+# # Atualizar o preço do produto com id 2 para 90
+# for produto in produtos:
+#     if produto["id"] == 2:
+#         produto["preço"] = 90
 
-print(produtos)
+# print(produtos)
+
+#===============================================
+
+# Dados dois dicionários, fundi-los em um único dicionário.
+
+dicionario1 = {"a": 1, "b": 2}
+dicionario2 = {"c": 3, "d": 4}
+
+dicionario_fundido = {**dicionario1, **dicionario2}
+
+print(dicionario_fundido)
