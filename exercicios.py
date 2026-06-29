f6fd81bacde11fd77f9f99f29979ea420ab312c4
João Henrique Sgobero <joao.sgobero@gmail.com>
2026-06-29 08:52:39 -0400

    Subindo aprendizado de 'Função com parametro e sem retorno'

--- exercicios.py
+++ exercicios.py
@@ -1,7 +1,19 @@
 #Função sem parametro e sem retorno:
 
-def diga_oi():
-    print("Oi")
-    # poderia usar algo como pass também
+# def diga_oi():
+#     print("Oi")
+#     # poderia usar algo como pass também
     
-diga_oi()
+# diga_oi()
+
+# ----------------------------------------------------------------------------
+
+#Função com parametro mas sem retorno:
+
+def saudacao(nome_visitante):
+    print(f"Seja bem vindo(a), {nome_visitante}")
+    
+
+saudacao("João")
+saudacao("Paula")
+
