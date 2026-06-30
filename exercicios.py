cde7d593fe9a766aa78caa220f2fc0e12bfed58e
João Henrique Sgobero <joao.sgobero@gmail.com>
2026-06-29 21:51:54 -0400

    Subindo aprendizado de função 'sem parametro e com retorno'

--- exercicios.py
+++ exercicios.py
@@ -10,10 +10,24 @@
 
 #Função com parametro mas sem retorno:
 
-def saudacao(nome_visitante):
-    print(f"Seja bem vindo(a), {nome_visitante}")
+# def saudacao(nome_visitante):
+#     print(f"Seja bem vindo(a), {nome_visitante}")
     
 
-saudacao("João")
-saudacao("Paula")
+# saudacao("João")
+# saudacao("Paula")
 
+# ----------------------------------------------------------------------------
+
+#Função sem parametro e com retorno
+
+from datetime import datetime
+
+
+def hora_certa():
+    agora = datetime.now()
+    return f"{agora.hour:02d}:{agora.minute:02d}:{agora.second:02d}"
+
+
+hora = hora_certa()
+print(f"{hora}")
